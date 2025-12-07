7822a1ca236f7e3b5c63d1c4c79ff3890dde40f8
Arnel <167875243+Arnel-rah@users.noreply.github.com>
2025-12-07 09:22:37 +0300

    feat: introduce ThemedText component using variant-based StyleSheet

--- app/components/ThemedText.tsx
+++ app/components/ThemedText.tsx
@@ -1,13 +1,46 @@
-import { Text,TextProps } from "react-native"
+import { StyleSheet, Text, TextProps } from "react-native";
+
+const styles = StyleSheet.create({
+  body3: {
+    fontSize: 10,
+    lineHeight: 16,
+  },
+  caption: {
+    fontWeight: "regular",
+    lineHeight: 12,
+    fontSize: 8,
+  },
+  headline: {
+    fontSize: 24,
+    lineHeight: 32,
+    fontWeight: "bold",
+  },
+  subtitle1: {
+    fontWeight: "bold",
+    lineHeight: 16,
+    fontSize: 14,
+  },
+  subtitle2: {
+    fontWeight: "bold",
+    lineHeight: 16,
+    fontSize: 12,
+  },
+  subtitle3: {
+    fontWeight: "bold",
+    lineHeight: 16,
+    fontSize: 10,
+  },
+});
 
 type Props = TextProps & {
-    variant?: string
-    color?: string
-}
+  variant?: keyof typeof styles;
+  color?: string;
+};
 
- const ThemedText = ({variant, color, ...rest}: Props) => {
-    return <Text {...rest}/>
+const ThemedText = ({ variant, color, ...rest }: Props) => {
+  return <Text style= {styles[variant ?? "body3"]} {...rest} />;
+};
 
-}
 
-export default ThemedText
+
+export default ThemedText;
