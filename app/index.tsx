da54c7e47c5d7755c59238985408e4e67913b6d9
Arnel <167875243+Arnel-rah@users.noreply.github.com>
2025-12-11 10:49:18 +0300

    feat(ui): update home screen layout with image header and Card component

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,16 @@
 import  ThemedText  from "@/app/components/ThemedText";
 import { Colors } from "@/constants/Colors";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import Card from "./components/Card";
 
 export default function Index() {
   return (
     <SafeAreaView style= {[styles.container, {backgroundColor: Colors.light.tint} ]}>
-    <ThemedText variant="headline" color="grayWhite">Pokédex</ThemedText>
+      <View style={styles.header}>
+        <Image source={require("@/assets/images/pokeball.png")} width={24} height={24}/>
+        <ThemedText variant="headline" color="grayDark">Pokédex</ThemedText>
+      </View>
     </SafeAreaView>
   );
 }
@@ -14,5 +18,8 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  header: {
+    flexDirection: 'row'
   }
 })
